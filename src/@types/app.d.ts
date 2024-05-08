1a5b277cb090e8beba74588504165c87a9e4a968
zonin <business.kariukimartin@gmail.com>
2024-05-08 18:35:29 +0300

    context types

---
+++ src/@types/app.d.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    name: string | null,
+    avatar: string | null,
+    email: string | null
+}
+
+export type UserContextType = {
+    user: IUser,
+    updateUser: (user: IUser) => void;
+}
