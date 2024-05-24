cd7baade548d1b046b6d3dd9300b72eb369fc0b9
zonin <business.kariukimartin@gmail.com>
2024-05-24 08:52:59 +0300

    bug: onClick failure on chat box

--- src/apps/Chat/ChatBox/ChatBox.tsx
+++ src/apps/Chat/ChatBox/ChatBox.tsx
@@ -1,8 +1,5 @@
 import Message from "../components/Message";
-import { UserProps } from "../../../@types/chat";
 import { useContext } from "react";
-import { AppContext } from "../../../context/appContext";
-import { UserContextType } from "../../../@types/app";
 import MessageBox from "../components/MessageBox";
 import {
   ConversationContext,
@@ -17,13 +14,7 @@ function ChatBox() {
   const messages = activeConversation?.messages?.map((message) => (
     <Message message={message} key={message?.msg} />
   ));
-  const { user } = useContext(AppContext) as UserContextType;
-  let sender: UserProps | null = null;
-  if (activeConversation && activeConversation?.user_1?.email == user.email) {
-    sender = activeConversation?.user_2;
-  } else if (activeConversation) {
-    sender = activeConversation?.user_1;
-  }
+ 
   return (
     <div className={`w-full relative h-full`}>
       <Bar/>
