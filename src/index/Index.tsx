7a4d6982318cd344f2bce93b3e477c9b6fd35531
zonin <business.kariukimartin@gmail.com>
2024-05-30 13:30:23 +0300

    index page

--- src/index/Index.tsx
+++ src/index/Index.tsx
@@ -1,48 +1,59 @@
-import {
-  Button,
-  Image,
-  Text,
-  TextInput,
-} from "@mantine/core";
+import { Button, Image, Text, TextInput } from "@mantine/core";
 import logo from "../assets/mklogo.png";
 import { GithubIcon } from "@mantinex/dev-icons";
-import { IconKeyboard, IconSearch } from "@tabler/icons-react";
+import { IconArrowRight, IconKeyboard, IconSearch } from "@tabler/icons-react";
+import { useContext } from "react";
+import { AppContext } from "../context/appContext";
+import { UserContextType } from "../@types/app";
 function Index() {
+  const { user } = useContext(AppContext) as UserContextType;
   return (
-    <div className="h-screen bg-gray-950 overflow-hidden">
-      <div className="flex justify-between p-5 md:p-5 bg-slate-950 sticky top-0 left-0 right-0">
-        <div className="flex items-center content-center justify-between">
-          <div className=" h-10 md:h-20 pr-5">
-            <Image src={logo} className="h-10 md:h-20 " radius={"100%"} />
-          </div>
+    <div className="h-screen bg-hero-pattern overflow-hidden bg-cover bg-center">
+      <div style={{backgroundColor: "rgba(0, 0, 0, .7)"}} className=" sm:px-20 h-full">
+        <div className="flex justify-between p-5 md:p-5  sticky top-0 left-0 right-0">
+          <div className="flex items-center content-center justify-between">
+            <div className=" h-10 md:h-20 pr-5">
+              <Image src={logo} className="h-10 md:h-20 " radius={"100%"} />
+            </div>
 
-          <Text>Hello World</Text>
+            <Text>Hello World</Text>
+          </div>
+          <div className="flex items-center">
+            <TextInput
+              placeholder="search"
+              pr={10}
+              className="hidden md:block"
+              leftSection={<IconSearch />}
+              rightSection={
+                <Text size="xs" c="dimmed">
+                  ⌘K
+                </Text>
+              }
+            />
+            <GithubIcon size={30} />
+          </div>
         </div>
-        <div className="flex items-center">
-            <TextInput placeholder="search" pr={10} leftSection={<IconSearch />} rightSection={<IconKeyboard />}/>
-          <GithubIcon size={20} />
-        </div>
-      </div>
-      <div className="flex items-center h-3/5 p-5">
-        <div>
-          <p className="">
-            Bringing You A <br />New Experience In{" "} <br />
-            <span className="text-indigo"></span>{" "}
-            <Text
-              size="xl"
-              fw={900}
-              variant="gradient"
-              gradient={{ from: "yellow", to: "orange", deg: 177 }}
-              component="span"
+        <div className="flex items-center h-3/5 p-5">
+          <div>
+            <p className="my-5 text-md sm:font-black antialiased leading-4 font-mono md:text-4xl  ">
+              Bringing You a <br />
+              New Experience In <br />
+              <span className="text-orange-500">Communication</span> and{" "}
+              <span className="text-orange-500">Commerce</span>
+            </p>
+          
+            <hr className="w-3/5 mb-3 border-orange-400" />
+            <hr className="w-2/5 mb-3 border-orange-400" />
+            <hr className="w-1/5 mb-3 border-orange-400" />
+            <Button
+              variant="outline"
+              component="a"
+              href={user ? "/shop" : "/login"}
+              rightSection={<IconArrowRight size={20} />}
             >
-              Communication 
-            </Text>
-                {" "}and{" "}
-            <Text component="span" size="xl" fw={600} variant="gradient">
-              Commerce
-            </Text>
-          </p>
-          <Button variant="outline">Get Started</Button>
+              Get Started
+            </Button>
+          </div>
         </div>
       </div>
     </div>
