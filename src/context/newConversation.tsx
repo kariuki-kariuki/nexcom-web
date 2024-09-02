3ccb7f9d829ffd30f0cf3144c91fa9bd3c9a9489
Kariuki <mkmartinoes@gmail.com>
2024-09-02 21:01:02 +0300

    ft: handle new conversation

---
+++ src/context/newConversation.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState } from 'react';
+import { GlobalUser } from '../@types/chat';
+
+export type NewConversationType = {
+  newConversation: GlobalUser | null;
+  setNewConversation: (convo: any) => void;
+};
+
+export const NewConversationContext = createContext<NewConversationType | null>(
+  null,
+);
+
+const NewConversationProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [newConversation, setNewConversation] = useState<GlobalUser | null>(
+    null,
+  );
+
+  // const updateActiveConver = (active: ConversationProps) => {
+  //     setActiveConversation(active);
+  // }
+  return (
+    <NewConversationContext.Provider
+      value={{ newConversation, setNewConversation }}
+    >
+      {children}
+    </NewConversationContext.Provider>
+  );
+};
+
+export default NewConversationProvider;
