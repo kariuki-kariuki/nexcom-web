18585e22f954da299fc5c6b8e02dfd39ad913687
Kariuki <mkmartinoes@gmail.com>
2024-09-21 10:51:18 +0300

    fix: login issue

--- src/apps/Auth/Callback.tsx
+++ src/apps/Auth/Callback.tsx
@@ -1,40 +1,17 @@
-import axios from 'axios';
-import { useContext } from 'react';
+import { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { AppContext } from '../../context/appContext';
-import { UserContextType } from '../../@types/app';
-import { url } from '../../data/url';
 
 const Callback = () => {
   const navigate = useNavigate();
   const { token } = useParams<{ token: string }>(); // Ensure token is typed
-  const { updateUser } = useContext(AppContext) as UserContextType;
 
-  const fetchToken = async () => {
-    if (!token) {
-      console.log('Token is missing.');
-      navigate('/login', { replace: true });
-    } else {
+  useEffect(() => {
+    if (token) {
       localStorage.setItem('token', token);
-    }
-
-    try {
-      const response = await axios.get(`${url}/auth/me`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      updateUser(response.data);
       navigate('/', { replace: true });
-    } catch (error) {
-      console.error('Error fetching token:', error);
     }
-  };
-
-  fetchToken();
-
-  return <div>Callback</div>;
+  }, [token]);
+  return <div>Success</div>;
 };
 
 export default Callback;
