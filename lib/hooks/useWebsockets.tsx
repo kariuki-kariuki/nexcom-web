11b8af4923c4e0def3936128f6215cfeaa931b8b
Kariuki <mkmartinoes@gmail.com>
2025-02-23 14:43:51 +0300

    bug: websockets conditional pooling

--- lib/hooks/useWebsockets.tsx
+++ lib/hooks/useWebsockets.tsx
@@ -8,7 +8,7 @@ import { useSocketContext } from "./useSocket";
 
 const useWebSocket = () => {
   const { state, dispatch } = useChat();
-  const socket =  useSocketContext(); 
+  const socket = useSocketContext();
   const { user } = useGlobalContext();
   const { activeConversation, setActiveConversation } = useActiveConversation();
   const markMessageState = useCallback((state: MessageState, conversationId: string) => {
@@ -25,7 +25,7 @@ const useWebSocket = () => {
           markMessageState(MessageState.READ, res.conversation.id)
         } else {
           const conv = state.conversations.find(conv => conv.id === res.conversation.id);
-          if(conv){
+          if (conv) {
             markMessageState(MessageState.DELIVERED, res.conversation.id)
           }
         }
@@ -40,26 +40,29 @@ const useWebSocket = () => {
   }, [dispatch])
 
   const handleNewConversation = useCallback((res: ConversationProps) => {
-    if(res.users[0].id === user?.id || res.users[1].id === user?.id) {
+    if (res.users[0].id === user?.id || res.users[1].id === user?.id) {
       res.users = res.users.filter(usr => usr.id !== user.id);
       dispatch({ type: 'ADD_CONVERSATION', payload: res })
     }
-    if(res.users[0].id === user?.id) {
+    if (res.users[0].id === user?.id) {
       res.users = [res.users[1]];
-      setActiveConversation(res)}
+      setActiveConversation(res)
+    }
   }, [dispatch, user]);
 
   useEffect(() => {
     // Listener for message-state updates
-    socket.emit('join');
-    // Attach socket event listeners
-    socket.on("message-state", handleMessageState);
-    socket.on("message", handleIncomingMessage);
-    socket.on('new-conversation', handleNewConversation)
-    socket.on("error", (e) => {
-      console.error("Socket error:", e);
-    });
 
+    if (user) {
+      socket.emit('join');
+      // Attach socket event listeners
+      socket.on("message-state", handleMessageState);
+      socket.on("message", handleIncomingMessage);
+      socket.on('new-conversation', handleNewConversation)
+      socket.on("error", (e) => {
+        console.error("Socket error:", e);
+      });
+    }
     // Clean up the socket listeners on component unmount
     return () => {
       socket.off("message-state", handleMessageState);
@@ -68,7 +71,7 @@ const useWebSocket = () => {
     };
   }, [socket, dispatch, handleIncomingMessage, handleMessageState]);
 
-  return { state};
+  return { state };
 };
 
 export default useWebSocket;
