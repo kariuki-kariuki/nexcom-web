9640749371fe806b662fc003f5f42955bb3c7f76
Kariuki <mkmartinoes@gmail.com>
2025-05-09 18:24:52 +0300

    fix: next config

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -14,16 +14,16 @@ export default withBundleAnalyzer({
   },
   redirects() {
     return [
-      {
-        source: '/videos',
-        destination: '/videos/tags/all',
-        permanent: false
-      },
-      {
-        source: '/videos/tag',
-        destination: '/videos/tags/all',
-        permanent: false
-      },
+      // {
+      //   source: '/videos',
+      //   destination: '/videos/tags/all',
+      //   permanent: false
+      // },
+      // {
+      //   source: '/videos/tag',
+      //   destination: '/videos/tags/all',
+      //   permanent: false
+      // },
       {
         source: '/business/product',
         destination: '/business/product/tags/all',
