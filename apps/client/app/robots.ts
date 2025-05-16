58ceca991829737a782fdc410fda1c0adb81e974
Kariuki <mkmartinoes@gmail.com>
2025-05-16 08:44:03 +0300

    fix: robots file

--- apps/client/app/robots.ts
+++ apps/client/app/robots.ts
@@ -1,13 +1,15 @@
 import { siteMetadata } from '@/lib/data/siteMetadata'
 import type { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/dashboard/', '/chats/'],
+      },
+    ],
     sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
   }
 }
