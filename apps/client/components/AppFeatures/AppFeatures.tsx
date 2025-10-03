1bfaa81343e23c20717bca3c305f6af4ce339587
Kariuki <mkmartinoes@gmail.com>
2025-10-03 13:17:17 +0200

    feat: landing page features section

---
+++ apps/client/components/AppFeatures/AppFeatures.tsx
@@ -0,0 +1,65 @@
+import { Container, Flex, Paper, Text, Title } from '@mantine/core'
+import React from 'react'
+import AppFeaturesCard, { AppFeaturesCardProps } from './AppFeaturesCard';
+import classes from "./AppFeaturesCard.module.css"
+
+const appFeatures: AppFeaturesCardProps[] = [
+    {
+        title: "End-to-End Encrypted Chat",
+        image: "/e2e.png",
+        content:
+            "Our chat system is built with end-to-end encryption, ensuring that only you and your intended recipient can read the messages. Not even our servers have access — your conversations remain private and secure.",
+        tags: ["security", "privacy", "messaging", "E2EE"],
+    },
+    {
+        title: "Group Chat Made Simple",
+        image: "/group-chat.png",
+        content:
+            "Connect with friends, family, or your entire team in secure group chats. Share messages, media, and ideas in real time. Organize communities, collaborate on projects, or just have fun chatting together.",
+        tags: ["group chat", "collaboration", "real-time", "media sharing"],
+    },
+    {
+        title: "Shopping & Business Made Easy",
+        image: "/shopping.png",
+        content:
+            "Launch your own shop in just a few clicks. Manage products, update prices, and keep your catalog fresh with an intuitive interface. Customers can chat & inquire about items in real time, while you manage your business seamlessly from one place.",
+        tags: ["ecommerce", "business", "dashboard", "real-time chat", "scalability"],
+    },
+    {
+        title: "Performance Dashboard & Analytics",
+        image: "/dashboard.png",
+        content:
+            "Stay on top of performance with an interactive dashboard. Track sales, growth, and customer engagement with powerful analytics that help you make smarter decisions.",
+        tags: ["analytics", "dashboard", "growth tracking", "insights"],
+    },
+    {
+        title: "Seamless Community & Marketplace",
+        image: "/community.png",
+        content:
+            "Bring together people, products, and conversations in one place. Whether it’s for social communities, project collaboration, or marketplace interactions — the app adapts to your needs.",
+        tags: ["community", "marketplace", "integration", "all-in-one"],
+    },
+    {
+        title: "Seamless Payments",
+        image: "/fintech.png",
+        content:
+            "Accept payments securely from anywhere in the world. With support for mobile money, cards, and digital wallets, you can provide customers with flexible, fast, and trusted payment options.",
+        tags: ["payments", "fintech", "checkout", "secure", "global"],
+    },
+];
+
+const AppFeatures = () => {
+    return (
+        <Paper className={classes.features}>
+        <header>
+            <Title ta="center" py="md" c="coco.5" className={classes.title}>Features</Title>
+            <Text ta="center" mb="xl">What we offer on the go!</Text>
+        </header>
+        <Flex direction={{ base: "column", sm: "row" }} wrap="wrap" justify={"space-between"}>
+            {appFeatures.map((feature) => <AppFeaturesCard key={feature.title} props={feature} />)}
+        </Flex>
+        </Paper>
+    )
+}
+
+export default AppFeatures
---
+++ apps/client/components/AppFeatures/AppFeaturesCard.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { Avatar, Card, CardSection, Divider, List, ListItem, Paper, Text, ThemeIcon, Title, useMantineColorScheme } from '@mantine/core'
+import { IconCircleCheck } from '@tabler/icons-react';
+import React from 'react'
+import classes from "./AppFeaturesCard.module.css"
+import { MotionWrapper } from '../ui/MotionWrapper';
+
+export interface AppFeaturesCardProps {
+    title: string;
+    image: string;
+    content: string;
+    tags: string[];
+}
+
+const AppFeaturesCard = ({ props }: { props: AppFeaturesCardProps }) => {
+    const { colorScheme } = useMantineColorScheme()
+    const srcRoute = colorScheme === "dark" ? "/images/features" : "/images/features/light"
+    return (
+        <MotionWrapper className={classes.card} direction='up'>
+            <Card radius={20} bg="none">
+                <CardSection>
+                    <Avatar w="100%" radius={0} h={'auto'} src={srcRoute + props.image} name={props.title} />
+                </CardSection>
+                <CardSection>
+                    <Divider />
+                </CardSection>
+                <CardSection p="xl" withBorder>
+                    <Title order={3} c="coco.5">{props.title}</Title>
+                    <Paper py="md" bg="none">
+                        <Text c="dimmed">{props.content}</Text>
+                    </Paper>
+                    <List spacing="xs"
+                        size='sm'
+                        icon={
+                            <ThemeIcon color='coco.5' size={28} radius="xl">
+                                <IconCircleCheck size={16} />
+                            </ThemeIcon>
+                        }
+                    >
+                        {props.tags.map((tag) => <ListItem key={tag}>{tag}</ListItem>)}
+                    </List>
+                </CardSection>
+            </Card>
+        </MotionWrapper>
+    )
+}
+
+export default AppFeaturesCard
